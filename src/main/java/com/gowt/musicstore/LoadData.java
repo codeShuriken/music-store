ac4499e3aae24f6eddfc37f068754fe891b6fbfd
Gowtham Vuppala <gowthamvuppala1@gmail.com>
2021-04-13 14:49:52 -0400

    Changed database configurations

--- src/main/java/com/gowt/musicstore/LoadData.java
+++ src/main/java/com/gowt/musicstore/LoadData.java
@@ -1,5 +1,32 @@
 package com.gowt.musicstore;
 
-public class LoadData {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.gowt.musicstore.entities.User;
+import com.gowt.musicstore.repositories.UserRepository;
+
+@Configuration
+public class LoadData {
+	  private static final Logger log = LoggerFactory.getLogger(LoadData.class);
+	  
+	  @Autowired
+	  private  PasswordEncoder passwordEncoder;
+	  
+	  @Bean
+	  CommandLineRunner initDatabase(UserRepository repository) {
+
+	    return args -> {
+	    	User user = repository.findByUsername("admin");
+	    	if (user == null) {
+	  	      log.info("Preloading " + repository.save(new User("admin", "abc123@gmail.com", passwordEncoder.encode("admin"), 5186456585L, 
+	  	    		  "ROLE_USER;ROLE_ADMIN")));
+	    	}
+	    };
+	  }
 }
